6df76a3def4a656d02370dc77a7908dc5942f4fb
Serge Gotsuliak <sgotsulyak@gmail.com>
2024-07-14 14:44:19 +0300

    ..

--- src/llamafactory/hparams/model_args.py
+++ src/llamafactory/hparams/model_args.py
@@ -54,7 +54,7 @@ class ModelArguments:
         metadata={"help": "Where to store the pre-trained models downloaded from huggingface.co or modelscope.cn."},
     )
     use_fast_tokenizer: bool = field(
-        default=True, # default=False, # gotzmann
+        default=True,
         metadata={"help": "Whether or not to use one of the fast tokenizer (backed by the tokenizers library)."},
     )
     resize_vocab: bool = field(
